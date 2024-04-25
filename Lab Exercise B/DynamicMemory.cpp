58bbd57a33f12f8b6dcad608b9cd268ca2293bd9
“dumi299” <bsc-com-14-21@unima.ac.mw>
2024-04-25 21:33:18 +0200

    Git

---
+++ Lab Exercise B/DynamicMemory.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int *dynamicInteger = new int;
+    string *dynamicString = new string;
+
+    
+    
+    return 0;
+}
