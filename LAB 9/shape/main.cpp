34c6d7d8cc9bc5c6cb21bd0e555adeb546d638af
“dumi299” <bsc-com-14-21@unima.ac.mw>
2024-06-10 18:04:51 +0200

    3rd commit

--- LAB 9/shape/main.cpp
+++ LAB 9/shape/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main() {
 Rectangle rect;
 Triangle trgl;
-rect.SetValues(4,5);
-trgl.SetValues (4,5);
-    cout << rect.Area() << '\n';
-    cout << trgl.Area() << '\n';
-
+Polygon* pPoly1 = &rect;
+Polygon* pPoly2 = &trgl;
+pPoly1->SetValues(4,5);
+pPoly2->SetValues(4,5);
+cout << rect.Area() << '\n';
+cout << trgl.Area() << '\n';
 return 0;
 }
